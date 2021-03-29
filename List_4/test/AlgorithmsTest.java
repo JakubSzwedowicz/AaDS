1a6db5dc6a4fac9e5a124a99204318aa2ff59fab
Jakub Szwedowicz <kuba.szwedowicz@gmail.com>
2021-03-29 18:20:43 +0200

    Changing algorithm and its tests so it could work with every Comparable type

--- List_4/test/AlgorithmsTest.java
+++ List_4/test/AlgorithmsTest.java
@@ -11,19 +11,19 @@ class AlgorithmsTest {
 
     @ParameterizedTest(name = "kthSmallest run {index}")
     @MethodSource("AlgorithmsTest_Provider#testKthSmallest_AllKth_Provider")
-    void testKthSmallest_AllKth(int[] tab_param, int kth_param, int answer) {
+    <T extends Comparable<? super T>> void testKthSmallest_AllKth(T[] tab_param, int kth_param, int answer) {
         Assertions.assertEquals(answer, Algorithms.kthSmallest(tab_param, kth_param));
     }
 
     @ParameterizedTest(name = "kthSmallest run {index}")
     @MethodSource("AlgorithmsTest_Provider#testKthSmallest_SpecialCases_Provider")
-    void testKthSmallest_SpecialCases(int[] tab_param, int kth_param, int answer) {
+    <T extends Comparable<? super T>> void testKthSmallest_SpecialCases(T[] tab_param, int kth_param, int answer) {
         Assertions.assertEquals(answer, Algorithms.kthSmallest(tab_param, kth_param));
     }
 
     @ParameterizedTest(name = "kthSmallest run {index}")
     @MethodSource("AlgorithmsTest_Provider#testKthSmallest_IllegalArgumentsException_Provider")
-    void testKthSmallest_IllegalArgumentsException(int[] tab_param, int kth_param) {
+    <T extends Comparable<? super T>> void testKthSmallest_IllegalArgumentsException(T[] tab_param, int kth_param) {
         Exception ex = Assertions.assertThrows(IllegalArgumentException.class, () -> Algorithms.kthSmallest(tab_param, kth_param));
         Assertions.assertEquals("Illegal parameters were passed!", ex.getMessage());
     }
--- List_4/test/AlgorithmsTest_Provider.java
+++ List_4/test/AlgorithmsTest_Provider.java
@@ -11,39 +11,39 @@ public class AlgorithmsTest_Provider {
 
     public static Stream<Arguments> testKthSmallest_AllKth_Provider(){
         return Stream.of(
-                Arguments.of(new int[]{5, 2, 4, 6, 8, 6, 3}, 1, 2),
-                Arguments.of(new int[]{5, 2, 4, 6, 8, 6, 3}, 2, 3),
-                Arguments.of(new int[]{5, 2, 4, 6, 8, 6, 3}, 3, 4),
-                Arguments.of(new int[]{5, 2, 4, 6, 8, 6, 3}, 4, 5),
-                Arguments.of(new int[]{5, 2, 4, 6, 8, 6, 3}, 5, 6),
-                Arguments.of(new int[]{5, 2, 4, 6, 8, 6, 3}, 6, 6),
-                Arguments.of(new int[]{5, 2, 4, 6, 8, 6, 3}, 7, 8)
+                Arguments.of(new Integer[]{5, 2, 4, 6, 8, 6, 3}, 1, 2),
+                Arguments.of(new Integer[]{5, 2, 4, 6, 8, 6, 3}, 2, 3),
+                Arguments.of(new Integer[]{5, 2, 4, 6, 8, 6, 3}, 3, 4),
+                Arguments.of(new Integer[]{5, 2, 4, 6, 8, 6, 3}, 4, 5),
+                Arguments.of(new Integer[]{5, 2, 4, 6, 8, 6, 3}, 5, 6),
+                Arguments.of(new Integer[]{5, 2, 4, 6, 8, 6, 3}, 6, 6),
+                Arguments.of(new Integer[]{5, 2, 4, 6, 8, 6, 3}, 7, 8)
         );
     }
 
     public static Stream<Arguments> testKthSmallest_SpecialCases_Provider(){
         return Stream.of(
-                Arguments.of(new int[]{1, 2, 3, 4, 5}, 1, 1),
-                Arguments.of(new int[]{5, 4, 3, 2, 1}, 1, 1),
-                Arguments.of(new int[]{1, 2}, 1, 1),
-                Arguments.of(new int[]{1, 2}, 2, 2),
-                Arguments.of(new int[]{1, 1, 1, 1}, 1, 1),
-                Arguments.of(new int[]{1, 1, 2, 2, 3, 3}, 1, 1),
-                Arguments.of(new int[]{1, 1, 2, 2, 3, 3}, 2, 1),
-                Arguments.of(new int[]{1, 1, 2, 2, 3, 3}, 3, 2),
-                Arguments.of(new int[]{1, 1, 2, 2, 3, 3}, 4, 2),
-                Arguments.of(new int[]{1, 1, 2, 2, 3, 3}, 5, 3),
-                Arguments.of(new int[]{1, 1, 2, 2, 3, 3}, 6, 3)
+                Arguments.of(new Integer[]{1, 2, 3, 4, 5}, 1, 1),
+                Arguments.of(new Integer[]{5, 4, 3, 2, 1}, 1, 1),
+                Arguments.of(new Integer[]{1, 2}, 1, 1),
+                Arguments.of(new Integer[]{1, 2}, 2, 2),
+                Arguments.of(new Integer[]{1, 1, 1, 1}, 1, 1),
+                Arguments.of(new Integer[]{1, 1, 2, 2, 3, 3}, 1, 1),
+                Arguments.of(new Integer[]{1, 1, 2, 2, 3, 3}, 2, 1),
+                Arguments.of(new Integer[]{1, 1, 2, 2, 3, 3}, 3, 2),
+                Arguments.of(new Integer[]{1, 1, 2, 2, 3, 3}, 4, 2),
+                Arguments.of(new Integer[]{1, 1, 2, 2, 3, 3}, 5, 3),
+                Arguments.of(new Integer[]{1, 1, 2, 2, 3, 3}, 6, 3)
         );
     }
     public static Stream<Arguments> testKthSmallest_IllegalArgumentsException_Provider(){
         return Stream.of(
-                Arguments.of(new int[]{}, 1),
+                Arguments.of(new Integer[]{}, 1),
                 Arguments.of(null, 1),
-                Arguments.of(new int[]{1, 2}, 3),
-                Arguments.of(new int[]{1, 2}, 5),
-                Arguments.of(new int[]{1, 2}, -1),
-                Arguments.of(new int[]{1, 2}, 0)
+                Arguments.of(new Integer[]{1, 2}, 3),
+                Arguments.of(new Integer[]{1, 2}, 5),
+                Arguments.of(new Integer[]{1, 2}, -1),
+                Arguments.of(new Integer[]{1, 2}, 0)
         );
     }
 
